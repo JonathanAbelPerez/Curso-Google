4662596323fe916f6d40e0a6b763c4f4ece261da
Jonathan Perez <jonathanabelerez@gmail.com>
2021-12-13 17:26:01 -0300

    implemento de navegacion entre fragmentos

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,11 +14,22 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,
         container, false)
+
         binding.playButton.setOnClickListener{ view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+
+        binding.aboutButton.setOnClickListener{ view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_aboutFragment)
+        }
+
+        binding.rulesButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_rulesFragment)
+        }
+
         setHasOptionsMenu(true)
         return binding.root
     }
